0de42753b8aca784c675f0e3d02ca256b1274490
R-Hari-Narayan <harinarayan2002@gmail.com>
2025-09-26 11:42:05 +0530

    LC394.py completed

--- LC394.py
+++ LC394.py
@@ -3,19 +3,33 @@
 def decodeString(s: str) -> str:
     stack = []
     output = ""
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    isOpen = 0
     for ch in s:
-        if ch != "]":
-            stack.append(ch)
+        if (ch not in numbers) and (len(stack) == 0):
+            output+= ch
         else:
-            tmp = []
-            while stack[-1] != "[":
-                tmp.insert(0, stack.pop())
-            tmp = "".join(tmp)
-            stack.pop()
-            output += int(stack.pop()) * tmp
+            if ch != "]":
+                if ch == "[":
+                    isOpen += 1
+                stack.append(ch)
+            else:
+                isOpen -= 1
+                tmp = []
+                while stack[-1] != "[":
+                    tmp.insert(0, stack.pop())
+                tmp = "".join(tmp)
+                stack.pop()
+                number = []
+                while len(stack)!= 0 and stack[-1] in numbers:
+                    number.insert(0, stack.pop())
+                number = int("".join(number))
+                if isOpen:
+                    stack.append(number * tmp)
+                else:
+                    output += number * tmp
         print(stack)
-                
     return output
 
-s= "3[a2[c]]"
+s= "abc3[cd]xyz"
 print(decodeString(s))
