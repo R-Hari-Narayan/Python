2ecafbb8bcf8781efa8a774f2613eee5a566e432
R-Hari-Narayan <harinarayan2002@gmail.com>
2025-11-02 13:25:43 +0530

    LC198.py

---
+++ LC198.py
@@ -0,0 +1,18 @@
+# House robber
+
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return max(nums)
+        if len(nums) == 3:
+            return max(nums[0]+ nums[2], nums[1])
+        nums[2] += nums[0]
+        for i in range(3, len(nums)):
+            nums[i] += max(nums[i-2], nums[i-3])
+        return max(nums[-1], nums[-2])
+    
+sol = Solution()
+nums = [2,1,1,2]
+print(sol.rob(nums))
